530af8448e30189f5e533a4e68b58f1b573ae314
Angus Lin <lonesirius@gmail.com>
2022-04-23 09:51:23 +0100

    finished soduku solver

--- exercise/soduku.py
+++ exercise/soduku.py
@@ -8,18 +8,19 @@ import os
 
 class soduku:
     def __init__(self, puzzle) -> list:
-        self.puzzle = puzzle
-        self.avail = []
-        self.trial = []
+        self._puzzle = puzzle
+        self._avail = []
+        self._trial = []
+        self.available()
 
     def isSolved(self) -> bool:
         """returns True if puzzle is solved, False otherwise
         """
         # sumcol = [0] * 9
-        for row in self.puzzle:
+        for row in self._puzzle:
             if sum(row) != 45:
                 return False
-        for col in zip(*self.puzzle):
+        for col in zip(*self._puzzle):
             if sum(col) != 45:
                 return False
         return True
@@ -29,11 +30,13 @@ class soduku:
         """
         for i in range(9):
             for j in range(9):
-                if (not self.puzzle[i][j]) and (not self.avail[i][j]):
+                if (not self._puzzle[i][j]) and (not self._avail[i][j]):
                     return True
         return False
 
     def solve(self):
+        """solve the soduku puzzle
+        """
         while self.solve_fillsingle():
             pass
         while not self.isSolved():
@@ -43,39 +46,43 @@ class soduku:
         print(f"Puzzle failure : {self.solvedFail()}")
 
     def available(self):
-        if not self.avail:
+        """generate available options for puzzle
+        """
+        if not self._avail:
             # for j in range(9):
             #     self.avail.append([[1, 2, 3, 4, 5, 6, 7, 8, 9]
             #                       for i in range(9)])
-            self.avail = [copy.deepcopy(
+            self._avail = [copy.deepcopy(
                 [[1, 2, 3, 4, 5, 6, 7, 8, 9] for i in range(9)]) for j in range(9)]
 
             for i in range(9):
                 for j in range(9):
-                    if self.puzzle[i][j]:
-                        self.puzzle_fill(i, j, self.puzzle[i][j])
+                    if self._puzzle[i][j]:
+                        self.puzzle_fill(i, j, self._puzzle[i][j])
 
     def avail_row(self, row) -> list:
         """return available options in row
         """
         result = []
         for i in range(9):
-            result.extend(self.avail[row][i])
+            result.extend(self._avail[row][i])
         return result
 
     def avail_col(self, col) -> list:
-        """
+        """return available options in column
         """
         result = []
         for i in range(9):
-            result.extend(self.avail[i][col])
+            result.extend(self._avail[i][col])
         return result
 
-    def avail_q(self, q):
+    def avail_q(self, q) -> list:
+        """return available options in quadrant q
+        """
         result = []
         for i in range((q // 3) * 3, (q // 3) * 3 + 3):
             for j in range((q % 3) * 3, (q % 3) * 3 + 3):
-                result.extend(self.avail[i][j])
+                result.extend(self._avail[i][j])
         return result
 
     def avail_first(self):
@@ -83,12 +90,14 @@ class soduku:
         """
         for i in range(9):
             for j in range(9):
-                if self.avail[i][j]:
-                    return([i, j, self.avail[i][j][0]])
+                if self._avail[i][j]:
+                    return([i, j, self._avail[i][j][0]])
 
     def avail_print(self):
+        """return available options in a string
+        """
         result = ''
-        for row in self.avail:
+        for row in self._avail:
             for item in row:
                 temp = ""
                 for a in item:
@@ -98,100 +107,102 @@ class soduku:
         return result
 
     def solve_fillsingle(self):
+        """solve the puzzle by filling in only available option
+        Returns: True if at least one fill, False otherwise
+        """
         result = False
         for i in range(9):
             for j in range(9):
-                if len(self.avail[i][j]) == 1:
-                    self.puzzle_fill(i, j, self.avail[i][j][0])
+                if len(self._avail[i][j]) == 1:
+                    self.puzzle_fill(i, j, self._avail[i][j][0])
                     result = True
         for q in range(9):
-            print(f'processing {q}...')
             countx = [(self.avail_row(q)).count(i+1) for i in range(9)]
             while 1 in countx:
                 idx = countx.index(1)
                 countx[idx] = 0
                 for i in range(9):
-                    if (idx + 1) in self.avail[q][i]:
+                    if (idx + 1) in self._avail[q][i]:
                         self.puzzle_fill(q, i, idx+1)
                         result = True
                         break
-            print(self)
-            print(self.avail_print())
 
             county = [(self.avail_col(q)).count(i+1) for i in range(9)]
             while 1 in county:
                 idx = county.index(1)
                 county[idx] = 0
                 for i in range(9):
-                    if (idx + 1) in self.avail[i][q]:
+                    if (idx + 1) in self._avail[i][q]:
                         self.puzzle_fill(i, q, idx+1)
                         result = True
                         break
 
-            print(self)
-            print(self.avail_print())
-            print(self.avail_q(q))
             countq = [(self.avail_q(q)).count(i+1) for i in range(9)]
-            print(countq)
             while 1 in countq:
                 idx = countq.index(1)
                 countq[idx] = 0
                 for i in range((q // 3) * 3, (q // 3) * 3 + 3):
                     for j in range((q % 3) * 3, (q % 3)*3 + 3):
-                        if (idx+1) in self.avail[i][j]:
+                        if (idx+1) in self._avail[i][j]:
                             self.puzzle_fill(i, j, idx+1)
                             result = True
                             break
                     else:
                         continue
                     break
-            print(self)
-            print(self.avail_print())
         return result
 
     def solve_trial(self) -> bool:
+        """Try to fill in the puzzle one by one.
+        """
+        # try:
         [x, y, data] = self.avail_first()
-        self.trial.append([x, y, data, copy.deepcopy(
-            self.puzzle), copy.deepcopy(self.avail)])
+        self._trial.append([x, y, data, copy.deepcopy(
+            self._puzzle), copy.deepcopy(self._avail)])
         self.puzzle_fill(x, y, data)
         while self.solve_fillsingle():
-            pass
-        if self.isSolved():
-            return True
-        if self.solvedFail():
-            #   solve fail, revert
-            self.puzzle = copy.deepcopy(self.trial[-1][-2])
-            self.avail = copy.deepcopy(self.trial[-1][-1])
-            self.avail[self.trial[-1][0]][self.trial[-1]
-                                          [1]].remove(self.trial[-1][2])
-            self.trial.pop()
-            self.solve_trial()
-        else:
-            self.solve_trial()
+            if self.isSolved():
+                return True
+            if self.solvedFail():
+                #   solve fail, revert
+                self._puzzle = copy.deepcopy(self._trial[-1][-2])
+                self._avail = copy.deepcopy(self._trial[-1][-1])
+                self._avail[self._trial[-1][0]][self._trial[-1]
+                                                [1]].remove(self._trial[-1][2])
+                self._trial.pop()
+            else:
+                self.solve_trial()
+        # except:
+        #     self._puzzle = copy.deepcopy(self._trial[-1][-2])
+        #     self._avail = copy.deepcopy(self._trial[-1][-1])
+        #     self._avail[self._trial[-1][0]][self._trial[-1]
+        #                                     [1]].remove(self._trial[-1][2])
+        #     self._trial.pop()
 
     def puzzle_fill(self, x: int, y: int, data: int):
         """fill x,y of the puzzle with data, update available accordingly
         """
         if not data:
             return
-        print(f"filling {x}{y} with {data}")
-        self.puzzle[x][y] = data
-        self.avail[x][y] = []
+        self._puzzle[x][y] = data
+        self._avail[x][y] = []
         for i in range(9):
-            if data in self.avail[x][i]:
-                (self.avail[x][i]).remove(data)
-            if data in self.avail[i][y]:
-                (self.avail[i][y]).remove(data)
+            if data in self._avail[x][i]:
+                (self._avail[x][i]).remove(data)
+            if data in self._avail[i][y]:
+                (self._avail[i][y]).remove(data)
         qx = x // 3
         qy = y // 3
         for i in range(qx*3, qx*3+3):
             for j in range(qy*3, qy*3+3):
-                if data in self.avail[i][j]:
-                    (self.avail[i][j]).remove(data)
+                if data in self._avail[i][j]:
+                    (self._avail[i][j]).remove(data)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Return the puzzle in string
+        """
         result = ''
-        for i, row in enumerate(self.puzzle):
+        for i, row in enumerate(self._puzzle):
             if i == 3 or i == 6:
                 result += '\n'
             for j, item in enumerate(row):
@@ -239,7 +250,6 @@ def main(fname=""):
     starttime = time.time()
     solver = soduku(puzzle)
     print(solver)
-    solver.available()
     solver.solve()
     endtime = time.time()
     print('\n')
